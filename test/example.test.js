584ef8e01f1e32601f578979ec6515aa5a3be6c4
James Keicher <jamesdkeicher@gmail.com>
2021-12-09 10:36:00 -0800

    Added better testing

--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,6 @@
 // IMPORT MODULES under test here:
 
-import { renderMushroom } from '../render-utils.js';
+import { renderMushroom, renderFriend } from '../render-utils.js';
 
 // import { example } from '../example.js';
 
@@ -22,11 +22,33 @@ test('time to test a function', (expect) => {
 });
 
 test('Should verify function returns an Object with a class of "mushroom"', (expect) => {
-    const expected = 'object';
-    const actual = typeof renderMushroom();
-    expect.equal(expected, actual);
+    const div = document.createElement('div');
+    div.classList.add('mushroom');
+    const expected = div;
+    const actual = renderMushroom();
+    expect.deepEqual(expected, actual);
 
     const expected1 = 'mushroom';
     const actual1 = renderMushroom();
     expect.equal(expected1, actual1.classList);
+});
+
+test('should do a thing', (expect) => {
+
+    let obj = {
+        name: 'Jack',
+        satisfaction: 2,
+    };
+    
+    const div = document.createElement('div');
+    div.classList.add('friend');
+    const nameSpan = document.createElement('span');
+    nameSpan.textContent = 'Jack';
+    const faceSpan = document.createElement('span');
+    faceSpan.textContent = '😐';
+    div.append(nameSpan, faceSpan);
+
+    const expected = div;
+    const actual = renderFriend(obj);
+    expect.deepEqual(expected, actual);
 });
